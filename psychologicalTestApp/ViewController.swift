4c96209d7c9fa431f56414d861952e10a795dba8
Huscro <huscro86512@gmail.com>
2022-07-25 19:13:33 +0800

    Initial Commit

---
+++ psychologicalTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  psychologicalTestApp
+//
+//  Created by 曾曜澤 on 2022/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
